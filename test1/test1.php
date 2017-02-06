4137e670c11409b306a416922bf88c016decb897
guoruibiao <marksinoberg@gmail.com>
2017-02-06 09:46:13 +0800

    init this repository named PFSI

---
+++ test1/test1.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ${郭璞}
+ * Date: 2017/2/6
+ * Time: 9:38
+ * Description:
+ */
+
+function show($var) {
+    if(isset($var)) {
+        var_dump($var);
+    }
+}
---
+++ test2/test2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ${郭璞}
+ * Date: 2017/2/6
+ * Time: 9:38
+ * Description:
+ */
+
+function time(){
+    echo date('y-m-d D');
+}
